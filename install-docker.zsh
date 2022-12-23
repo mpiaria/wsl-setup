7f643567a4e983558d41171385669014f0cad12a
Michael Iaria <8315738+mpiaria@users.noreply.github.com>
2022-12-22 23:30:50 -0600

    Create install-docker.zsh

---
+++ install-docker.zsh
@@ -0,0 +1,39 @@
+#!/bin/zsh
+
+sudo apt remove docker docker.io containerd runc;
+
+sudo apt update;
+
+sudo apt install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release;
+
+sudo mkdir -p /etc/apt/keyrings;
+
+if [ ! -f /etc/apt/keyrings/docker.gpg ]; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg;
+fi
+
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
+
+sudo apt update;
+
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin;
+
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy;
+sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy;
+
+if ! command sudo grep -qc "service docker start" "/etc/wsl.conf"; then
+    echo "Adding docker service to wsl.conf";
+    echo "" | sudo tee -a /etc/wsl.conf;
+    echo "[boot]" | sudo tee -a /etc/wsl.conf;
+    echo "command=service docker start" | sudo tee -a /etc/wsl.conf;
+else
+    echo "Docker service is already in wsl.conf";
+fi
+
+sudo apt install -y docker-compose;
